d704cb0d221e0a00749f3e192082056b07721e29
Mayur <surendramayur@gmail.com>
2018-10-30 17:15:02 +0530

    getapp module

--- src/components/GetApp/GetApp.js
+++ src/components/GetApp/GetApp.js
@@ -7,11 +7,11 @@ const getApp = () => (
     <div className={classes.GetApp}>
         <img src={deviceImg} alt="device" />
         <div>
-            <a href = "https://play.google.com/store/apps/details?id=com.infin.petsearch&hl=en" >
-            Get the App 
-            <div><img src={googlePlayImg} alt="GooglePlay"/></div>
-            </a>
-        </div>        
+        <img src={googlePlayImg} alt="GooglePlay"/>
+        <a href = "https://play.google.com/store/apps/details?id=com.infin.petsearch&hl=en" >
+            Get the App     
+        </a> 
+        </div>                  
     </div>
 );
 
