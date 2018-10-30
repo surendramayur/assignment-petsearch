5e186536c8038edc1a489e0fb03b84fa1812713e
Mayur <surendramayur@gmail.com>
2018-10-30 14:00:42 +0530

    css modules enabled and basic cleaning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Something</div>
+      <div className={classes.App}>React-App Initialized</div>
     );
   }
 }
