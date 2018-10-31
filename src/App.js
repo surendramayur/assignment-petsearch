d1d3d357faebbb4cdfe0f77b3f7fcf690f7eadcf
Mayur <surendramayur@gmail.com>
2018-10-31 12:44:37 +0530

    deployment try1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import classes from './App.css';
 import Homepage from './containers/Homepage/Homepage';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <div className={classes.App}>        
           <Homepage />        
         </div>
-      </BrowserRouter>      
+      </HashRouter>      
     );
   }
 }
